24421a8f16e434fdc96a3532b88e03929c586ea3
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2023-04-08 17:42:26 +0300

    put

--- src/api.js
+++ src/api.js
@@ -1,29 +1,36 @@
-const cors = require('cors');
-const express = require('express');
-const serverless = require('serverless-http');
+const cors = require("cors");
+const express = require("express");
+const serverless = require("serverless-http");
 
 const app = express();
 
 const router = express.Router();
 
 let counter = 5;
+let show = counter;
 
-router.get('/', (req, res) => {
-    counter++;
+router.get("/", (req, res) => {
+  res.json({
+    show,
+  });
+});
 
-    res.json({
-        counter,
-    });
+router.put("/", (req, res) => {
+  counter++;
+  res.json({
+    counter,
+    show,
+  });
 });
 
 const corsOptions = {
-    origin: '*',
-    methods: 'GET,POST',
-    optionsSuccessStatus: 204
+  origin: "*",
+  methods: "GET,PUT",
+  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
 
-app.use('/.netlify/functions/api', router);
+app.use("/.netlify/functions/api", router);
 
 module.exports.handler = serverless(app);
