97104070113b69bbd8fecb001ec1c880cf03821d
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2023-04-08 18:48:38 +0300

    fix

--- src/api.js
+++ src/api.js
@@ -6,26 +6,65 @@ const app = express();
 
 const router = express.Router();
 
-let counter = 5;
+let facebookcounter = 5;
+let twittercounter = 5;
+let googlecounter = 5;
+let symbaloocounter = 5;
+let pinterestcounter = 5;
+let emailcounter = 5;
 
 router.get("/", (req, res) => {
-  res.json({
-    counter,
-  });
+    res.json({
+        facebookcounter,
+        twittercounter,
+        googlecounter,
+        symbaloocounter,
+        pinterestcounter,
+        emailcounter,
+    });
 });
 
-router.put("/", (req, res) => {
-  counter++;
-  res.json({
-    counter,
-    
-  });
+router.put("/twitter", (req, res) => {
+    twittercounter++;
+    res.json({
+        twittercounter,
+    });
+});
+router.put("/facebook", (req, res) => {
+    facebookcounter++;
+    res.json({
+        facebookcounter,
+    });
+});
+router.put("/google", (req, res) => {
+    googlecounter++;
+    res.json({
+        googlecounter,
+    });
+});
+router.put("/symbaloo", (req, res) => {
+    symbaloocounter++;
+    res.json({
+        symbaloocounter,
+    });
+});
+router.put("/pinterest", (req, res) => {
+    pinterestcounter++;
+    res.json({
+        pinterestcounter,
+    });
+});
+router.put("/email", (req, res) => {
+    emailcounter++;
+    res.json({
+        emailcounter,
+    });
 });
 
 const corsOptions = {
-  origin: "*",
-  methods: "GET,PUT",
-  optionsSuccessStatus: 204,
+    origin: "*",
+    methods: "GET,PUT",
+    optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
