3ec81017f5a660f5d9f6cdc630cd43eefc5defaf
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2023-04-08 19:19:48 +0300

    something added

--- src/api.js
+++ src/api.js
@@ -13,16 +13,37 @@ let symbaloocounter = 5;
 let pinterestcounter = 5;
 let emailcounter = 5;
 
-router.get("/", (req, res) => {
+router.get("/facebook", (req, res) => {
     res.json({
-        facebookcounter,
-        twittercounter,
-        googlecounter,
-        symbaloocounter,
-        pinterestcounter,
-        emailcounter,
+        facebookcounter
     });
 });
+router.get("/twitter", (req, res) => {
+    res.json({
+        twittercounter
+    });
+});
+router.get("/google", (req, res) => {
+    res.json({
+        googlecounter
+    });
+});
+router.get("/symbaloo", (req, res) => {
+    res.json({
+        symbaloocounter
+    });
+});
+router.get("/pinterest", (req, res) => {
+    res.json({
+        pinterestcounter
+    });
+});
+router.get("/email", (req, res) => {
+    res.json({
+        emailcounter
+    });
+});
+
 
 router.put("/twitter", (req, res) => {
     twittercounter++;
